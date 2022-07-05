f30416178882ceb4f66179df3684b44e45472b37
Niklas Porsche <niklas.porsche@student.tugraz.at>
2022-07-05 11:30:41 -0700

    added settings to examples

--- PolygonGame1/__main__.py
+++ PolygonGame1/__main__.py
@@ -2,9 +2,11 @@ from __future__ import annotations
 
 import json
 import cv2
+import yaml
 
 from PolygonGame1.TensorboardLogger1 import TensorboardLogger1
 from MonteScene.MonteCarloTreeSearch import MonteCarloSceneSearch
+from MonteScene.utils import convert_dict2namespace
 
 from .PolygonGame1 import PolygonGame1
 
@@ -13,6 +15,7 @@ def main():
         #directories
         input_path = "input/"
         output_path = "PolygonGame1/output/"
+        settings_path = "PolygonGame1/settings.yaml"
 
         #load data
         with open(f'{input_path}truth.json', "r") as t:
@@ -22,11 +25,15 @@ def main():
 
         image: cv2.Mat = cv2.imread(f'{input_path}image.jpg')
 
+        with open(settings_path, 'r') as f:
+                settings = yaml.safe_load(f)
+        settings = convert_dict2namespace(settings)
+
 
         #mcss
         environment = PolygonGame1(truth_json, proposals_json, image)
         logger = TensorboardLogger1(environment, output_path)
-        mcss = MonteCarloSceneSearch(environment, mcts_logger=logger)
+        mcss = MonteCarloSceneSearch(environment, mcts_logger=logger, settings=settings)
 
         mcss.run()
 
--- PolygonGame2/__main__.py
+++ PolygonGame2/__main__.py
@@ -2,10 +2,12 @@ from __future__ import annotations
 
 import json
 import cv2
+import yaml
 
 from Proposals.LabeledPolygonPool import LabeledPolygonPool
 from PolygonGame2.TensorboardLogger2 import TensorboardLogger2
 from MonteScene.MonteCarloTreeSearch import MonteCarloSceneSearch
+from MonteScene.utils import convert_dict2namespace
 
 from PolygonGame2.PolygonGame2 import PolygonGame2
 
@@ -14,6 +16,7 @@ def main():
         #directories
         input_path = "input/"
         output_path = "PolygonGame2/output/"
+        settings_path = "PolygonGame1/settings.yaml"
 
         #load data
         with open(f'{input_path}truth.json', "r") as t:
@@ -26,11 +29,15 @@ def main():
         segmentation[:] = 0
         label_to_color_map = LabeledPolygonPool.from_json(truth_json).draw_to(segmentation, { None : [0, 0, 0] }) #generate semantic segmentation
 
+        with open(settings_path, 'r') as f:
+                settings = yaml.safe_load(f)
+        settings = convert_dict2namespace(settings)
+
 
         #mcss
         environment = PolygonGame2(segmentation, label_to_color_map, proposals_json)
         logger = TensorboardLogger2(environment, output_path)
-        mcss = MonteCarloSceneSearch(environment, mcts_logger=logger)
+        mcss = MonteCarloSceneSearch(environment, mcts_logger=logger, settings=settings)
 
         mcss.run()
 
